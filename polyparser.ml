f320852a19a68a7a04a57e812a257ffed43903d0
Anders Fugmann <af@issuu.com>
2016-04-25 15:54:30 +0200

    Updated

--- polyparser.ml
+++ polyparser.ml
@@ -1,8 +1,11 @@
 let rec eval = function
-  | `Number n -> n
+  | `Int n -> n
   | `Binop (`Plus, a, b) -> eval a + eval b
   | `Binop (`Mult, a, b) -> eval a * eval b
 
+let to_string n =
+  string_of_int n
+
 let () =
-  let expr = `Binop(`Plus, `Number 1, `Binop(`Plus, `Number 1, `Number 5)) in
-  Printf.printf "%d\n" (eval expr)
+  let expr = `Binop(`Plus, `Int 1, `Binop(`Plus, `Int 1, `Int 5)) in
+  Printf.printf "%s\n" (to_string (eval expr))
